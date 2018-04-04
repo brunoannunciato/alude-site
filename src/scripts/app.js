53d65e90c89b4de644561221a782ac3b5d9f8050
Bruno Annunciato <brunobannunciato@gmail.com>
2018-04-04 18:10:16 -0300

    first commit

---
+++ src/scripts/app.js
@@ -0,0 +1,9 @@
+'use strict';
+
+// require('fullpage.js/dist/jquery.fullpage.min.js');
+
+var fullpage = require('modules/fullpage');
+
+$(function () {
+     // fullpage();
+});
---
+++ src/scripts/modules/fullpage.js
@@ -0,0 +1,13 @@
+'use strict';
+
+module.exports = function () {
+    
+    $(document).ready(function() {
+        $('#alude').fullpage({
+            anchors:['home', 'ouca', 'alude']
+
+
+        });
+    });
+
+}
